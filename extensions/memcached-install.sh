f4652fa1897c55f699147033d21288f46b41efb6
Konrad <kg@bwteam.pl>
2017-09-07 10:38:51 +0200

    Update memcached-install.sh

--- extensions/memcached-install.sh
+++ extensions/memcached-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Enable the module
-echo "extension=memcached.so" | tee -a /usr/local/php7/etc/conf.d/modules.ini
+echo "extension=memcache.so" | tee -a /usr/local/php7/etc/conf.d/modules.ini
