b9383658efb21fe5e2a333821425f63ea5cf5355
Konrad <kg@bwteam.pl>
2017-09-07 12:26:36 +0200

    Update memcached-build.sh

--- extensions/memcached-build.sh
+++ extensions/memcached-build.sh
@@ -6,7 +6,6 @@ apt-get update
 apt-get install -y \
     libmemcached-dev \
 
-cd /usr/src
 wget https://github.com/websupport-sk/pecl-memcache/archive/NON_BLOCKING_IO_php7.zip
 unzip NON_BLOCKING_IO_php7.zip
 cd pecl-memcache-NON_BLOCKING_IO_php7
